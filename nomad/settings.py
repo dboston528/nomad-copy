2c0b05a52c1abeb3ae9c0e5a51873cb3922124a9
DeAndre Boston <dboston3@yahoo.com>
2019-08-21 12:58:22 -0500

    Update settings

--- nomad/settings.py
+++ nomad/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^6h6_h650=!#6t24!z7tn(&6t75gctsl^fso=#^%53tyicfvq)'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
